c3339033f6d49c90f82b82ca4f7390a95bc6aa3c
Nefedova <Katerina163>
2021-05-18 16:10:55 +0400

    2. Парсинг HTML страницы. [475130]

---
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,26 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.SortedMap;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element td : row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+        }
+        Elements data = doc.select(".altCol");
+        for (Element td : data) {
+            if (!td.attr("style").isEmpty()) {
+                System.out.println(td.text());
+            }
+        }
+    }
+}
